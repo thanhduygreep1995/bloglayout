829628910a7cb6ce460ab161abbff7800469b7e2
thanhduygreep1995 <thanhduygreep1995@gmail.com>
2023-09-20 22:28:46 +0700

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins'],
+        'Roboto': ['Roboto'],
+      }
+    },
+  },
+  plugins: [],
+}
+
